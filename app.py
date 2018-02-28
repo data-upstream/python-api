edc7e66ed7b4665bab116f42a252ddee90febe27
Alex <info@sp33c.de>
2018-02-28 03:53:37 +0100

    downsample

--- app.py
+++ app.py
@@ -12,13 +12,21 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
-@app.route('/api/v1.0/pandas/ts/downsample', methods=['POST'])
-def convert_timeseries():
+@app.route('/api/v1.0/pandas/ts/downsample/<range>', methods=['POST'])
+def convert_timeseries(range):
     if not request.json:
         abort(400)
-    
-    ts = pd.Series(request.json.get('data'), index=request.json.get('index'))
-    return ts.to_json(), 200
+
+    try:
+        index = request.json.get('index')
+        ts = pd.Series(request.json.get('data'), list(map(lambda x: pd.to_datetime(x), index)))
+        tsResample = ts.resample(range)
+        if 'mean' in request.args:
+            tsResample = ts.resample(range).mean()
+    except:
+        abort(400)
+
+    return tsResample.to_json(), 200
 
 
 if __name__ == '__main__':
